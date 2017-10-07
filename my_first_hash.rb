19be8ef6c78c1b029e25ffe500ec697a44cc9b30
Araba Taylor <araba.taylor20@gmail.com>
2017-10-07 22:07:39 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,11 +9,11 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  retrieval["oil paintings"]
+  shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  shipping_manifest["pearl necklance"]=1
+  shipping_manifest["pearl necklace"]=1
   shipping_manifest
 end
