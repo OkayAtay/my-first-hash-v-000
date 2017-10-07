38babdd3f684d37ee4102cf161b017e81fbe9e19
Araba Taylor <araba.taylor20@gmail.com>
2017-10-07 22:06:34 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,8 +14,7 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here
-  #remember to return the shipping_manifest hash
+  shipping_manifest["pearl necklance"]=1
+  shipping_manifest
 
 end
